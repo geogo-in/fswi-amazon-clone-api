f461c4abad227387b540fc686e5cc62e3d5f0234
Meet <meetmakwana203.@gmail.com>
2022-01-26 21:47:10 +0530

    Added some comments

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,25 +1,31 @@
 const req = require('express/lib/request');
 const multer = require('multer')
 
+// The disk storage engine gives you full control on storing files to disk.
 const storage = multer.diskStorage({
-  destination: function(req, file, callback) {
+  // destination - The folder to which the file has been saved
+  destination: function (req, file, callback) {
     callback(null, './uploads');
   },
-  filename: function(req, file, callback) {
+  // filename - The name of the file within the destination
+  filename: function (req, file, callback) {
+    // callback - null means noe error, 2nd parameter gives unique name for image file
     callback(null, new Date().toISOString() + file.originalname);
   }
 })
 
 const fileFilter = (req, file, callback) => {
-  // reject a file
+  // accept the file
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
     callback(null, true);
-  } else {
-    callback(null, false);    
+  }
+  // reject the file
+  else {
+    callback(null, false);
   }
 }
-const upload = multer({ 
-  storage: storage, 
+const upload = multer({
+  storage: storage,
   limits: {
     fileSize: 2 * 1024 * 1024 // 2MB only
   },
@@ -29,7 +35,7 @@ const upload = multer({
 const productsController = require('../controllers/productsController');
 
 module.exports = (app) => {
-  app.get('/', function(req, res, next) {
+  app.get('/', function (req, res, next) {
     res.send({ ping: 'FSWI Amazon Clone API' });
   });
   //adaptive practive routes 
